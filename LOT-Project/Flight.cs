28556f0333fee429c2f1b0273a858679865c5f5e
DjDecay21 <mrychert12@gmail.com>
2024-04-17 09:52:38 +0200

    DjDecay(laptop)

---
+++ LOT-Project/Flight.cs
@@ -0,0 +1,15 @@
+﻿namespace LOT_Project
+{
+    public class Flight
+    {
+        public int id {  get; set; }
+        public string flightNumber { get; set; }
+
+        public DateTime departureDate {  get; set; }
+
+        public string departurePoint {  get; set; }
+        public string arrivalPoint { get; set; }
+        public string aircraftType { get; set; }
+
+    }
+}
---
+++ LOT-Project/Migrations/20240416121018_FirstMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LOT_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    flightNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    departureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    departurePoint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    arrivalPoint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AircraftType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
